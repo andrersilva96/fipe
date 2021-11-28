f1f1fc366f2fa377ea58a8dc22f35131e5ce2cea
André Rodrigues <andre.rsilva96@hotmail.com>
2021-11-28 13:21:21 -0300

    translate to br

---
+++ resources/lang/pt-BR/roles.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'super' => 'Super Usuário',
+    'admin' => 'Administrador',
+    'manager' => 'Gerente',
+    'sales-man' => 'Vendedor',
+    'plural' => [
+        'super' => 'Super Usuários',
+        'admin' => 'Administradores',
+        'manager' => 'Gerentes',
+        'sales-man' => 'Vendedores',
+    ],
+    'description' => [
+        'super' => 'Ideal para o proprietário do CRM.',
+        'admin' => 'Ideal para clientes empresariais do CRM.',
+        'manager' => 'Ideal para os gerentes das empresas.',
+        'sales-man' => 'Ideal para vendedores das empresas.',
+    ]
+];
